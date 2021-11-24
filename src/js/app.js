388181cc66aca9f0239312cc48e19f7032272148
Yana Chumburidze <ya.chum@gmail.com>
2021-11-24 14:36:11 +0300

    remove extra code

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,9 @@
 // TODO: write code here
-
 // comment this to pass build
-// const unusedVariable = 'variable';
 
 // for demonstration purpose only
 export default function demo(value) {
-  return value;
+    return value;
 }
 
 // eslint-disable-next-line no-console
