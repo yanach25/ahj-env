c5842315b4b1ceb8f0279890ef4c69363ede8796
Yana Chumburidze <ya.chum@gmail.com>
2021-11-24 14:05:24 +0300

    lint:fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,12 @@
 // TODO: write code here
 
 // comment this to pass build
-const unusedVariable = 'variable';
+// const unusedVariable = 'variable';
 
 // for demonstration purpose only
 export default function demo(value) {
-    return value;
+  return value;
 }
 
+// eslint-disable-next-line no-console
 console.log('app.js included');
